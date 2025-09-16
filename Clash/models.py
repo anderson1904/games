537f1da2bf4c38e9f4d0bec15257a411dece2884
anderson1904 <andersoncosta190406@gmail.com>
2025-09-15 21:35:47 -0300

    first commit

---
+++ Clash/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class tbJogador(models.Model):
+    pass
+
+class tbPartida(models.Model):
+    pass
+
+class tbStream(models.Model):
+    pass
+
+class tbNoticia(models.Model):
+    pass   
+
+#relativos a compras e vendas
+class tbProduto(models.Model):
+    pass
+
+class tbCategoria(models.Model):
+    pass
+
+class tbCompra(models.Model):
+    pass
+
+class tbCarrinho(models.Model):
+    pass
+
